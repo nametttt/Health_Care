1f17267ec94880b25961beab69b979d4a3d23b8b
TatDep <gub.airat@ya.ru>
2024-02-03 18:16:44 +0300

    start

--- app/src/main/java/com/tanya/health_care/LoginActivity.java
+++ app/src/main/java/com/tanya/health_care/LoginActivity.java
@@ -3,12 +3,60 @@ package com.tanya.health_care;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    ImageButton imgBtn;
+    EditText password;
+
+    private boolean isVisible = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        init();
+    }
+
+    private void init(){
+        imgBtn = findViewById(R.id.eye);
+        password = findViewById(R.id.password);
+        password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                togglePassVisability();
+            }
+        });
+    }
+
+
+    private void togglePassVisability() {
+        if (isVisible) {
+            String pass = password.getText().toString();
+            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            password.setText(pass);
+            imgBtn.setBackgroundResource(R.drawable.eye);
+            password.setSelection(pass.length());
+        } else {
+            String pass = password.getText().toString();
+            password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            password.setInputType(InputType.TYPE_CLASS_TEXT);
+            password.setText(pass);
+            imgBtn.setBackgroundResource(R.drawable.eye_off);
+
+            password.setSelection(pass.length());
+        }
+        isVisible= !isVisible;
     }
 }
